63df9a8dba5b041161729a2fd0679e7f7d7b5eae
Zach Schneider <me@zachschneider.com>
2023-08-01 16:43:51 -0500

    Update functions.php
    Already defined constants warning fix

--- wp-content/themes/twentytwenty-zkm/functions.php
+++ wp-content/themes/twentytwenty-zkm/functions.php
@@ -35,10 +35,15 @@ if ( is_admin() ) {
 // Add social icons.
 add_filter( 'twentytwenty_svg_icons_social', 'customize_social_icons' );
 function customize_social_icons( $icons ) {
-    // Define constants for social icons
-    define( 'ICON_STACKOVERFLOW', '<svg className="svg-icon" aria-hidden="true" role="img" focusable="false" width="24" height="24" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg"><path d="M189.4,629.2v65.3h315.2v-65.3H189.4z M194.8,511.9l-4.5,65.3
+    // Define constants for social icons if not already defined
+    if ( ! defined( 'ICON_STACKOVERFLOW' ) ) {
+        define( 'ICON_STACKOVERFLOW', '<svg class="svg-icon" aria-hidden="true" role="img" focusable="false" width="24" height="24" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg"><path d="M189.4,629.2v65.3h315.2v-65.3H189.4z M194.8,511.9l-4.5,65.3
 	l314.4,21.3l4.1-64.9L194.8,511.9L194.8,511.9z M97.9,484.8V800H150H563h33.2V484.8h-52.1v262.7H150V484.8H97.9L97.9,484.8z M218.6,373.1L203,436.3l305.8,75.9l15.6-63.6L218.6,373.1z M286.3,215.1l-33.2,56.2L524.8,431l33.2-56.6L286.3,215.1z M452.1,65.3 l-54.2,36.5l175.3,261.5l54.2-36.1L452.1,65.3L452.1,65.3z M663.9,0l-64.9,7.8l38.2,312.8l64.9-7.8L663.9,0z" /></svg>' );
-    define( 'ICON_TWITTER', '<svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M14.258 10.152L23.176 0h-2.113l-7.747 8.813L7.133 0H0l9.352 13.328L0 23.973h2.113l8.176-9.309 6.531 9.309h7.133zm-2.895 3.293l-.949-1.328L2.875 1.56h3.246l6.086 8.523.945 1.328 7.91 11.078h-3.246zm0 0"></path></svg>' );
+    }
+    
+    if ( ! defined( 'ICON_TWITTER' ) ) {
+        define( 'ICON_TWITTER', '<svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M14.258 10.152L23.176 0h-2.113l-7.747 8.813L7.133 0H0l9.352 13.328L0 23.973h2.113l8.176-9.309 6.531 9.309h7.133zm-2.895 3.293l-.949-1.328L2.875 1.56h3.246l6.086 8.523.945 1.328 7.91 11.078h-3.246zm0 0"></path></svg>' );
+    }
 
     // Map social icons
     $social_icons = array(
@@ -59,4 +64,3 @@ function add_rel_attribute_to_social_menu_link( $atts, $item, $args, $depth ) {
     }
     return $atts;
 }
-
